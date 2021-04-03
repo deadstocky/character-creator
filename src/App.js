81193cf8d1215bb5d9ad0b34a5231efb07980191
Donovan Hong <donovanhong@gmail.com>
2021-04-03 15:04:28 -0700

    Exercise 4  Solution Updates.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
-import Backdrop from './components/Backdrop';
 
 function App() {
   return (
     <>
       <CharacterEditor />
-      <Backdrop />
       <Footer />
     </>
   );
